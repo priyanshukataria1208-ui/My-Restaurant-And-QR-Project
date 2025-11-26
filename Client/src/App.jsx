f252709d1d21675c725b7dbf5a22bada3dd51dc3
priyanshu <priyanshukataria1208@gmail.com>
2025-11-26 22:22:57 +0530

    Register & Login Page

--- Client/src/App.jsx
+++ Client/src/App.jsx
@@ -1,10 +1,23 @@
 import React from 'react'
 import Register from './Components/Register'
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Login from './Components/Login';
+import Navbar from './Components/Navbar';
+import UserProduct from './Components/UserProduct';
+
 
 const App = () => {
   return (
     <div>
-      <Register/>
+    <Router>
+      <Navbar/>
+      <Routes>
+<Route path='/Reg' element={<Register/>}/>
+<Route path='/Login' element={<Login/>}/>
+<Route path='/userproduct' element={<UserProduct/>}/>
+
+      </Routes>
+    </Router>
     </div>
   )
 }
--- Client/src/Components/Login.jsx
+++ Client/src/Components/Login.jsx
@@ -1,45 +1,52 @@
 import React, { useState } from "react";
-import toast from"react-hot-toast"
+import toast from "react-hot-toast";
 import {
   MDBInput,
   MDBCol,
+  MDBRow,
+  MDBCheckbox,
   MDBBtn,
   MDBIcon,
-  MDBRow,
-  MDBCheckbox,  
 } from "mdb-react-ui-kit";
 import { useNavigate } from "react-router-dom";
 
-const Register = () => {
-  const [name, setUsername] = useState("");
-  const [email, setEmail] = useState("");
+
+
+
+const Login = () => {
+  const [name, setName] = useState("");
   const [password, setPassword] = useState("");
   const navigate = useNavigate();
+  
 
-  const handleregister=(e)=> {
+  const handleLogin = (e) => {
     e.preventDefault();
 
-    fetch("http://localhost:5000/api/REGISTER", {
+    fetch("http://localhost:5000/api/login", {
       method: "POST",
       headers: { "Content-Type": "application/json" },
-      body: JSON.stringify({ name, email, password }),
+      body: JSON.stringify({ name, password }),
     })
       .then((res) => res.json())
       .then((data) => {
-        if (data.status === 201) {
-          toast.success(data.message);
-          navigate("/Login");
+        if (data.success) {
+        
+          setLoginName(data.apiData);
+
+          if (data.apiData === "Priyanshu") navigate("/userproduct");
+          else navigate("/");
+
+          toast.success("Login Successfully");
         } else {
           toast.error(data.message);
         }
       });
-
-  }
+  };
 
   return (
     <div id="reg-wrapper">
-      <form id="reg-box" onSubmit={handleregister}>
-        <h2 className="reg-title">✨ Registration Form</h2>
+      <form id="reg-box" onSubmit={handleLogin}>
+        <h2 className="reg-title">🔑 Login Form</h2>
 
         <section className="reg-inputs">
           <MDBInput
@@ -47,15 +54,7 @@ const Register = () => {
             type="text"
             label="Username"
             value={name}
-            onChange={(e) => setUsername(e.target.value)}
-            required
-          />
-          <MDBInput
-            className="mb-3 reg-field"
-            type="email"
-            label="Email"
-            value={email}
-            onChange={(e) => setEmail(e.target.value)}
+            onChange={(e) => setName(e.target.value)}
             required
           />
           <MDBInput
@@ -69,35 +68,55 @@ const Register = () => {
         </section>
 
         <MDBRow className="mb-3">
-          <MDBCol className="d-flex justify-content-center">
+          <MDBCol className="d-flex justify-content-start">
             <MDBCheckbox label="Remember me" defaultChecked />
           </MDBCol>
-          <MDBCol>
+          <MDBCol className="text-end">
             <a href="/forget">Forgot password?</a>
           </MDBCol>
         </MDBRow>
 
         <MDBBtn type="submit" className="reg-btn" block>
-          Sign Up
+          Login
         </MDBBtn>
 
         <div className="text-center mt-3">
           <p>
-            Already registered? <a href="/login">Login</a>
+            Don’t have an account? <a href="/reg">Register</a>
           </p>
-          <p>Or sign up with:</p>
+          <p>Or sign in with:</p>
 
           <div className="social-wrap">
-            <MDBBtn floating color="secondary" className="mx-1 social-btn">
+            <MDBBtn
+              floating
+              color="secondary"
+              className="mx-1 social-btn"
+              onClick={() => window.open("https://www.facebook.com")}
+            >
               <MDBIcon fab icon="facebook-f" />
             </MDBBtn>
-            <MDBBtn floating color="secondary" className="mx-1 social-btn">
+            <MDBBtn
+              floating
+              color="secondary"
+              className="mx-1 social-btn"
+              onClick={() => window.open("https://www.google.com")}
+            >
               <MDBIcon fab icon="google" />
             </MDBBtn>
-            <MDBBtn floating color="secondary" className="mx-1 social-btn">
+            <MDBBtn
+              floating
+              color="secondary"
+              className="mx-1 social-btn"
+              onClick={() => window.open("https://twitter.com")}
+            >
               <MDBIcon fab icon="twitter" />
             </MDBBtn>
-            <MDBBtn floating color="secondary" className="mx-1 social-btn">
+            <MDBBtn
+              floating
+              color="secondary"
+              className="mx-1 social-btn"
+              onClick={() => window.open("https://github.com")}
+            >
               <MDBIcon fab icon="github" />
             </MDBBtn>
           </div>
@@ -107,4 +126,4 @@ const Register = () => {
   );
 };
 
-export default Register;
+export default Login;
